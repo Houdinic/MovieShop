a5e57e206ae5c03b8c204833b0f82cd802265fa1
Marshall <363752922@qq.com>
2021-07-13 22:03:27 -0400

    all new files

---
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using ApplicationCore.RepositoryInterfaces;
+
+namespace Infrastructure.Repositories
+{
+    public class MovieRepository : IMovieRepository
+
+    {
+        List<Movie> IMovieRepository.GetHighest30GrossingMovies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Services;
+using ApplicationCore.ServiceInterfaces;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMovieService movieService;
+
+        public HomeController(IMovieService _movieservice)
+        {
+            movieService = _movieservice;
+        }
+
+        public IActionResult Index()
+        {   
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
